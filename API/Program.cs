4b53fdab8c6ba5d0d0d20e0795343df0ad211f6e
Maskz1 <o4d0b@outlook.com>
2020-05-21 22:49:04 -0700

    Applied Repository Pattern and extended product entity with relationships; Configured migrations

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,38 @@
+using System;
+using System.Threading.Tasks;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // Apply migration and create database at startup
+            var host = CreateHostBuilder(args).Build();
+            // using means any code runs inside of this is going to be disposed once finished
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -17,15 +18,23 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // Order does not matter
         // Dependency Injections Container: 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddDbContext<StoreContext>(option =>
                 option.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            // AddTransient - short lifetime; instantiated for an individual method and not the request itself
+            // AddSingleton - created when application starts and destroy until the application shotdown
+            // AddScoped - created when HTTP request comes in and create controller and repository; destroyed when 
+            // the request is finished
+            services.AddScoped<IProductRepository, ProductRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Order matters
         // For Middlewares:
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
