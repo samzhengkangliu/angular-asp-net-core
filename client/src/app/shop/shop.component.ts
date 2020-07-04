faae49ae74e53e9c07f69f4ece6ba7169d9e4d70
Sam Liu <o4d0b@outlook.com>
2020-07-03 18:40:59 -0700

    Hooked up filters API

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -13,6 +13,8 @@ export class ShopComponent implements OnInit {
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
+  brandIdSelected = 0;
+  typeIdSelected = 0;
 
   constructor(private shopService: ShopService) {}
 
@@ -23,7 +25,7 @@ export class ShopComponent implements OnInit {
   }
 
   getProducts(): void {
-    this.shopService.getProducts().subscribe(
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe(
       (response) => {
         this.products = response.data;
       },
@@ -36,7 +38,7 @@ export class ShopComponent implements OnInit {
   getBrands(): void {
     this.shopService.getBrands().subscribe(
       (response) => {
-        this.brands = response;
+        this.brands = [{id: 0, name: 'All'}, ...response];
       },
       (error) => {
         console.log(error);
@@ -47,11 +49,21 @@ export class ShopComponent implements OnInit {
   getTypes(): void {
     this.shopService.getTypes().subscribe(
       (response) => {
-        this.types = response;
+        this.types = [{id: 0, name: 'All'}, ...response];
       },
       (error) => {
         console.log(error);
       }
     );
+  }
+
+  onBrandSelected(brandId: number): void {
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number): void {
+    this.typeIdSelected = typeId;
+    this.getProducts();
   }
 }
