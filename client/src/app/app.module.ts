0f62245cb1aba5214a0e555e11c28bb4ee612330
Sam Liu <o4d0b@outlook.com>
2020-07-03 17:24:55 -0700

    Applied shop service and clean up app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +16,7 @@ import { CoreModule } from './core/core.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent],
