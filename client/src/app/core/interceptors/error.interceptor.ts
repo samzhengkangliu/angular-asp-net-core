69e333d3fcb3c4e832ff7791a54f84c78c117129
Sam Liu <o4d0b@outlook.com>
2020-07-09 19:29:11 -0700

    Added error toasters and improved internal server component

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -7,11 +7,12 @@ import {
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { Router } from '@angular/router';
+import { Router, NavigationExtras } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private toastr: ToastrService) {}
 
   intercept(
     request: HttpRequest<any>,
@@ -20,11 +21,24 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(
       catchError((error) => {
         if (error) {
+          if (error.status === 400) {
+            if (error.error.errors) {
+              throw error.error;
+            } else {
+              this.toastr.error(error.error.message, error.error.statusCode);
+            }
+          }
+          if (error.status === 401) {
+            this.toastr.error(error.error.message, error.error.statusCode);
+          }
           if (error.status === 404) {
             this.router.navigateByUrl('/not-found');
           }
           if (error.status === 500) {
-            this.router.navigateByUrl('/server-error');
+            const navigationExtras: NavigationExtras = {
+              state: { error: error.error },
+            };
+            this.router.navigateByUrl('/server-error', navigationExtras);
           }
           return throwError(error);
         }
