f4c5d68a2cacdab742bb8acd02e7994938570e0c
Sam Liu <o4d0b@outlook.com>
2020-07-09 19:00:19 -0700

    Added Error Interceptor and error component

---
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error) => {
+        if (error) {
+          if (error.status === 404) {
+            this.router.navigateByUrl('/not-found');
+          }
+          if (error.status === 500) {
+            this.router.navigateByUrl('/server-error');
+          }
+          return throwError(error);
+        }
+      })
+    );
+  }
+}
