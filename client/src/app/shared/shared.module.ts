c0dbd331b0df8ca6ddee5bd2036f0309dc52babf
Sam Liu <o4d0b@outlook.com>
2020-07-10 23:11:39 -0700

    Added a template home page

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { PaginingHeaderComponent } from './components/pagining-header/pagining-header.component';
 import { PagerComponent } from './components/pager/pager.component';
 
 @NgModule({
   declarations: [PaginingHeaderComponent, PagerComponent],
-  imports: [CommonModule, PaginationModule.forRoot()],
-  exports: [PaginationModule, PaginingHeaderComponent, PagerComponent],
+  imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
+  exports: [
+    PaginationModule,
+    PaginingHeaderComponent,
+    PagerComponent,
+    CarouselModule,
+  ],
 })
 export class SharedModule {}
