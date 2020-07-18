8bfd254cf5618a329ad1ed99c71472126a9b4ab0
Sam Liu <o4d0b@outlook.com>
2020-07-17 17:29:00 -0700

    Created basket module and added lazy loading to basket

---
+++ client/src/app/basket/basket-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BasketComponent } from './basket.component';
+
+const routes: Routes = [{ path: '', component: BasketComponent }];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class BasketRoutingModule {}
