3a59db494d9408954fd4d21799de21a9a1b30c9f
Sam Liu <o4d0b@outlook.com>
2020-07-27 01:35:19 -0700

    Add basket total component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,15 +4,17 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { PaginingHeaderComponent } from './components/pagining-header/pagining-header.component';
 import { PagerComponent } from './components/pager/pager.component';
+import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 
 @NgModule({
-  declarations: [PaginingHeaderComponent, PagerComponent],
+  declarations: [PaginingHeaderComponent, PagerComponent, OrderTotalsComponent],
   imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
   exports: [
     PaginationModule,
     PaginingHeaderComponent,
     PagerComponent,
     CarouselModule,
+    OrderTotalsComponent,
   ],
 })
 export class SharedModule {}
