8e32a0744eaa3aa020b18724e7a77c6e5200dc1c
Sam Liu <o4d0b@outlook.com>
2020-09-04 19:00:41 -0700

    Added Register feature

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.createRegisterForm();
   }
 
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      displayName: [null, [Validators.required]],
+      email: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'),
+        ],
+      ],
+      password: [null, [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe(
+      (response) => {
+        this.router.navigateByUrl('/shop');
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
