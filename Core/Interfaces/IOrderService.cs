4a98973cfed01052c76630633e661627d6c4445a
Sam Liu <o4d0b@outlook.com>
2020-09-08 22:23:56 -0700

    Added CreateOrderAsync method

--- Core/Interfaces/IOrderService.cs
+++ Core/Interfaces/IOrderService.cs
@@ -6,7 +6,7 @@ namespace Core.Interfaces
 {
     public interface IOrderService
     {
-        Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string basketId,
+        Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string basketId,
             Address shippingAddress);
         Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail);
         Task<Order> GetOrderByIdAsync(int id, string buyerEmail);
